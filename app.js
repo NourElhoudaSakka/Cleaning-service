f2d72ef048d59fed5732da0c194b5e97cfede711
NourElhoudaSakka <nourelhouda.sakka@gmail.com>
2023-12-22 23:56:03 +0100

    seperated js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+var lat = 48.852969;
+var lon = 2.349903;
+var macarte = null;
+
+function initMap() {
+
+    macarte = L.map('map').setView([lat, lon], 11);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(macarte);
+}
+window.onload = function () {
+    initMap();
+};
